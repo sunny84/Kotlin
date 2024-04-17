af3d91ad12a61a6dd0f012524c3cc310514a06ff
sunny84 <sonya8412@gmail.com>
2024-04-17 14:58:39 +0900

    initial commit for TheMovieApp

---
+++ TheMovieApp/settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+//    ext.kotlin_version = '1.3.50'
+//    ext.anko_version = '0.10.8'
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TheMovieApp"
+include(":app")
+ 
