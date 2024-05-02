6f31aa3d9a300f6b0223b3c4f248a90ce24ef72c
Sun <sonya8412@gmail.com>
2024-05-02 10:42:14 +0900

    성능 지연 목록 만들기 LazyColumn

--- BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -71,11 +73,11 @@ fun OnboardingScreen(
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it"}
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
+    LazyColumn (modifier = modifier.padding(vertical = 4.dp)) {
+        items (items = names) {
+            name -> Greeting(name = name)
         }
     }
 }
