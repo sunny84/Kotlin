280402ac6af79d49011cad3ae376bb1b67fb1df3
Sun <sonya8412@gmail.com>
2024-05-13 10:55:25 +0900

    remember

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -7,13 +7,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count: MutableState<Int> = mutableStateOf(0)
+        var count: MutableState<Int> = remember { mutableStateOf(0) }
         Text(text = "You've had ${count.value} glasses.")
         Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
