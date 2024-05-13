a245ec9b2586df557e65ed208ac7fdc68662a7ad
Sun <sonya8412@gmail.com>
2024-05-13 10:58:46 +0900

    Delegated properties 'by'

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -5,18 +5,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count: MutableState<Int> = remember { mutableStateOf(0) }
-        Text(text = "You've had ${count.value} glasses.")
-        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+        var count by remember { mutableStateOf(0) }
+        Text(text = "You've had ${count} glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
