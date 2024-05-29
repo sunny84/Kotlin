325527aec4c7ba17202859612e2c38e86a08a67e
Sun <sonya8412@gmail.com>
2024-05-29 16:59:02 +0900

    Compose 상태 다루기 및 재활용 가능한 카드 만들기

--- QRCodeReader/app/build.gradle.kts
+++ QRCodeReader/app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -47,6 +48,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+//    packagingOptions {
+//        excludes 'META-INF/DEPENDENCIES'
+//        excludes 'META-INF/AL2.0'
+//        excludes 'META-INF/LGPL2.1'
+//    }
 }
 
 dependencies {
@@ -59,6 +65,28 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.media3.transformer)
+    implementation(libs.play.services.mlkit.barcode.scanning)
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    // CameraX dependencies
+    implementation("androidx.camera:camera-core:1.3.3")
+    implementation("androidx.camera:camera-camera2:1.3.3")
+    implementation("androidx.camera:camera-lifecycle:1.3.3")
+    implementation("androidx.camera:camera-view:1.3.3")
+    implementation("androidx.camera:camera-extensions:1.3.3")
+
+    // ZXing dependencies for QR code scanning
+    implementation("com.google.zxing:core:3.5.3")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+
+    // Coroutine dependencies
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation(libs.compose.preview.renderer)
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
